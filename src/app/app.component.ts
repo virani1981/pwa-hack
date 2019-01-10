9a78b5435519e036407429cc6c8c25a74a2d4c30
Scott Hammon <scotth@IT1511531RICH.corp.aectech.com>
2019-01-10 12:41:29 -0600

    changed components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,22 +3,18 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { EntryModel } from "../app/models/entrymodel";
-import { FlashcardService } from "../app/services/flashcards/flashcard.service";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  private localeId: string = "en-us";
-  private entries: EntryModel[];;
-  public show: boolean = false;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    private flashcardService: FlashcardService
   ) {
     this.initializeApp();
     
@@ -32,25 +28,8 @@ export class AppComponent {
         this.statusBar.backgroundColorByHexString('#074f8b');
       }
     });
-    this.flashcardService.getCurrentCards().then((entries) => { this.entries = entries});
   }
 
-  removeEntry(entry: EntryModel){
-    this.flashcardService.removeCard(entry);
-  }
 
-  addEntry(word: string){
-    console.log(word);
-    this.flashcardService.addCard(this.localeId, word);
-    this.toggle();
-  }
-
-  setLocaleId(val: string){
-    this.localeId = val;
-  }
-
-  toggle() {
-    this.show = !this.show;
-  }
 
 }
